8cc9a764ba991c779d05b4393d9e227357461b33
aeremin <aeremin@maxus.ru>
2015-06-16 18:49:47 +0300

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*places)
     # code goes here
+	places.each_with_object([]) do |place, arr|
+	  self.each do |k,v| 
+	    arr << k if v == place
+	  end
+	end
   end
 end
