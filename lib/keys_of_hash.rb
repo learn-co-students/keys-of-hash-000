499d674f802a4758c0d688727af002104c4bd87d
allensoberano <allensoberano@gmail.com>
2015-09-07 16:10:58 -0700

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,31 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    new_array = []
+
+    # arguments.each do |x|
+    #   if self.has_value?(x)
+
+    #    new_array << self.keys
+    #   end
+    # end
+
+    self.each do |key, value|
+      if arguments.include?(value)
+        new_array << key
+      end
+    end
+
+    new_array
   end
 end
+
+
+  myHash = {
+    a: 1,
+     b: 2,
+     c: 3,
+     d: 1
+ }
+
+ puts myHash.keys_of(1)
