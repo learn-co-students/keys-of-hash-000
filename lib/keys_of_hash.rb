2908e27e461ea5e8d9f0f9030f9d004b239cf354
zacharyh <zacharythunt@gmail.com>
2016-06-05 14:02:24 -0700

    keys-of-hash-donezo

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    new_array = []
+    each do |key, value|
+    arguments.each do |arg|
+      new_array << key if value == arg
+    end
   end
+  new_array
+end
 end
