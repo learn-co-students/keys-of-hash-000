ab6b60134698a35960df63efc156e973fb2db2ef
Peter Prosol <peter.prosol@gmail.com>
2016-12-05 22:18:37 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    self.map do |key, value|
+      key if arguments.include?(value)
+    end.flatten.compact
   end
 end
